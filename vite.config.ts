ed51c1bdd192519a51b114a473f142a9025d0cbe
ClovisFCardoso <clovisfcardoso@yahoo.com>
2026-05-10 14:41:45 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import tsconfigPaths from 'vite-tsconfig-paths'; // 👈 Adicione esta linha
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     tanstackStart(), 
     nitro(), 
     tsconfigPaths(), // 👈 E esta linha também
+    tailwindcss(),
     viteReact()
   ],
 });
